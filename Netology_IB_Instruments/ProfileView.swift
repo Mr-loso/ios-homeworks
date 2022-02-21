826e19cacc03a9a3a1be3293c35821d8859832ba
Mr-loso <81354775+Mr-loso@users.noreply.github.com>
2022-02-21 03:02:05 +0300

    Initial commit

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Арсений Корнилов on 21.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBOutlet weak var labelDate: UILabel!
+    @IBOutlet weak var labelCity: UILabel!
+
+    @IBOutlet weak var ImageViewProfilePhoto: UIImageView!
+    
+    @IBOutlet weak var textDescription: UITextView!
+    
+}
--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let subview = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView {
+            self.view.addSubview(subview)
+            subview.frame = CGRect(x: 0, y: 20, width: 400, height: 600)
+        }
     }
-
 
 }
 
